2074c1ab752afc3e6616c97eac6848629375449f
Magofoco <francesco.stefani@outlook.com>
2024-02-15 18:32:28 +0000

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider, Text } from "@chakra-ui/react";
 import Pages from "./containers/Pages";
 import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import customTheme from "./customTheme";
 
 const { REACT_APP_WALLET_CONNECT_PROJECT_ID } = process.env;
-
-const projectId = REACT_APP_WALLET_CONNECT_PROJECT_ID || "";
 
 const mumbaiNetwork = {
   chainId: 80001,
@@ -25,14 +23,21 @@ const metadata = {
   icons: ["https://avatars.mywebsite.com/"],
 };
 
-createWeb3Modal({
-  ethersConfig: defaultConfig({ metadata }),
-  chains: [mumbaiNetwork],
-  projectId,
-  enableAnalytics: true,
-});
-
 const App = () => {
+  if (!REACT_APP_WALLET_CONNECT_PROJECT_ID) {
+    return (
+      <Text>
+        Something went wrong. Make sure "projectId" is defined in the .env
+      </Text>
+    );
+  }
+  createWeb3Modal({
+    ethersConfig: defaultConfig({ metadata }),
+    chains: [mumbaiNetwork],
+    projectId: REACT_APP_WALLET_CONNECT_PROJECT_ID,
+    enableAnalytics: true,
+  });
+
   return (
     <ChakraProvider theme={customTheme}>
       <BrowserRouter>
